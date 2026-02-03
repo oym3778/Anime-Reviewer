75a9420c1e3cec0e77f5a1a5b400c607e7cdead2
oym3778 <oym3778@g.rit.edu>
2026-02-03 14:59:54 -0500

    added some references, also added note about firefox

--- src/hooks/useUser.js
+++ src/hooks/useUser.js
@@ -36,3 +36,21 @@ export function useUser() {
 
   return { user, userData, loading };
 }
+
+/*
+  !!!NOTE!!!
+  Firefox has Enhanced Tracking Protection (ETP) meant to protect the user from trackers following you around colleecting info while you browse
+  however in order for our redirect to work we rely on that information, disable the for sign-up to properly work
+  we should figure out how to prevent this later so firefox can still be used.
+
+  https://stackoverflow.com/questions/41055699/why-does-firebase-auth-work-for-chrome-but-not-firefox
+
+  // I believe this fixes the firefox ETP issue, keeps persistence of browser sessions, 
+  // so if user closes browser out, we log them out
+  // review https://jorgevergara.co/blog/firebase-auth-persistence/ if changes arise
+
+  ;=;
+  :|
+  x-x
+  bruh
+*/
